cd24eab8006d443ddbc491c493fa4a55fd01def3
Elias <elias.aymonier.pro@gmail.com>
2025-04-15 22:14:36 +0200

    Fonction de connection : presque, ne fonctionne encore pas completement

--- backend/login.mjs
+++ backend/login.mjs
@@ -3,19 +3,19 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 
-
-// example create data
-// const data = {
-//     "password": "12345678",
-//     "passwordConfirm": "12345678",
-//     "email": "elias@example.com",
-// };
-
 export async function userRegister(data) {
     try {
         const record = await pb.collection('users').create(data);
-        console.log("c bon mec normalement")
+        console.log("Vous avez crée votre compte !")
     } catch (e) {
-        console.log(e);
+        console.error(e);
+    }
+}
+
+export async function userLogin(mail, mdp) {
+    try {
+        await pb.collection('users').authWithPassword(mail, mdp);
+    } catch (e) {
+        console.error(e);
     }
 }
